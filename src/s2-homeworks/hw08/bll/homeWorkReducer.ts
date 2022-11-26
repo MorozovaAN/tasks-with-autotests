7571bff7917caf20a9e10e43108e226202a3e8cc
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2022-11-26 21:37:31 +0400

    feat: add hw08 solution

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,28 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      let newState = [...state];
+      return newState.sort((a, b) => {
+        return action.payload === "up"
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      }); // need to fix
     }
-}
+    case "check": {
+      return state.filter((u: any) => u.age > 17); // need to fix
+    }
+    default:
+      return state;
+  }
+};
